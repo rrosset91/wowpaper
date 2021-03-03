38b5452529b93679eacfcca97557b9198798652c
Roger <rrosset91@gmail.com>
2021-03-03 15:02:52 -0300

    taggeamento funcionando e passando dados

--- client/index.js
+++ client/index.js
@@ -1,19 +1,20 @@
 var app = new Vue({
-  el: "#app",
-  data: {
-    message: "",
-    submittedMessage: "",
-  },
-  methods: {
-    submitMessage: function () {
-      this.submittedMessage = this.message;
-    },
-  },
-});
-
-var banner = new Vue({
-  el: "#banner",
-  data: {
-    message: "WowPaper.io",
-  },
+	el: "#app",
+	template: `	
+	<div>
+	<input id="tags" class="input-tags" type="text" data-role="taginput" data-random-color="true" data-max-tags="5" data-tag-trigger="Comma" v-model="tags"/>
+	<div class="submit-button">
+	<button v-on:click="submitData" class="button primary">Submit</button></div></div>
+	`,
+	data() {
+		return {
+			tags: ""
+		};
+	},
+	methods: {
+		submitData: function () {
+			let inputValues = document.querySelector("#tags").value;
+			console.log(inputValues);
+		}
+	}
 });
